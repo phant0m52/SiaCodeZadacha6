58a706ea2a302363389deb1263d5c03d9acce0ca
Goncharov Denis <dgoncharov1606@gmail.com>
2025-12-24 15:11:14 +0300

    Update Direction.java

--- src/primmaze/maze/Direction.java
+++ src/primmaze/maze/Direction.java
@@ -1,2 +1,20 @@
-package maze;public class Direction {
+package primmaze.maze;
+
+public enum Direction {
+    UP(-1, 0),
+    RIGHT(0, 1),
+    DOWN(1, 0),
+    LEFT(0, -1);
+
+    public final int dr;
+    public final int dc;
+
+    Direction(int dr, int dc) {
+        this.dr = dr;
+        this.dc = dc;
+    }
+
+    public Direction opposite() {
+        return values()[(ordinal() + 2) % 4];
+    }
 }
