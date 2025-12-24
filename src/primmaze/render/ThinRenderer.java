3d5c4163a9bfa6159c1b40635aa5ab18230a7ec5
Goncharov Denis <dgoncharov1606@gmail.com>
2025-12-24 15:12:11 +0300

    Update ThinRenderer.java

--- src/primmaze/render/ThinRenderer.java
+++ src/primmaze/render/ThinRenderer.java
@@ -1,2 +1,32 @@
-package primmaze.render;public class ThinRenderer {
+package primmaze.render;
+
+import primmaze.maze.Maze;
+
+public class ThinRenderer {
+
+    public static String render(Maze maze) {
+        int n = maze.size();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("+");
+        for (int c = 0; c < n; c++) sb.append("---+");
+        sb.append("\n");
+
+        for (int r = 0; r < n; r++) {
+            sb.append("|");
+            for (int c = 0; c < n; c++) {
+                sb.append("   ");
+                sb.append(maze.cell(r, c).walls[1] ? "|" : " ");
+            }
+            sb.append("\n");
+
+            sb.append("+");
+            for (int c = 0; c < n; c++) {
+                sb.append(maze.cell(r, c).walls[2] ? "---" : "   ");
+                sb.append("+");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
