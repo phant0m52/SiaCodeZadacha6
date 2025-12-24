6aa659018d17f9eea0366a7e64ab68fe1cdaea0e
Goncharov Denis <dgoncharov1606@gmail.com>
2025-12-24 15:12:35 +0300

    Update Main.java

--- src/primmaze/Main.java
+++ src/primmaze/Main.java
@@ -1,2 +1,12 @@
-package primmaze;public class Main {
+package primmaze;
+
+import javax.swing.SwingUtilities;
+import primmaze.gui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
 }
